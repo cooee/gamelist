c53c0c9ffc7ab5f89d819315ce21ff3af137312b
YuchengMo <YuchengMo@boyaa.com>
2024-03-25 23:49:27 +0800

    fix: 新增素材
    # <body>
    
    # <footer>
    
    # type 字段包含:
    # feat：新功能（feature）
    # fix：修补bug
    # docs：文档（documentation）
    # style： 格式（不影响代码运行的变动）
    # refactor：重构（即不是新增功能，也不是修改bug的代码变动）
    # test：增加测试
    # chore：构建过程或辅助工具的变动
    # scope用于说明 commit 影响的范围，比如数据层、控制层、视图层等等。
    # subject是 commit 目的的简短描述，不超过50个字符
    # Body 部分是对本次 commit 的详细描述，可以分成多行
    # Footer用来关闭 Issue或以BREAKING CHANGE开头，后面是对变动的描述、
    #       以及变动理由和迁移方法

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,20 +1,24 @@
+import { RES } from "./res";
+
+console.log(RES)
+
 export const PopularGames = [
     {
         "id": 1,
         "slug": "grand-theft-auto-v",
         "name": "星际霸主",
         "released": "2013-09-17",
-        "background_image": "https://api.multiavatar.com/星际霸主.png",
+        "background_image": RES.xjbz,
         "rating": 4.47,
         "rating_top": 5,
-        "link":"https://017.tools/data/User/boyaa/home/desktop/games/web-xjbz/index.html"
+        "link":"https://017.tools/data/User/boyaa/home/desktop/games/xjbz/web-mobile/index.html"
     },
     {
         "id": 2,
         "slug": "grand-theft-auto-v",
         "name": "篮球小天才",
         "released": "2013-09-17",
-        "background_image": "https://api.multiavatar.com/篮球小天才.png",
+        "background_image": RES.lq,
         "rating": 4.47,
         "rating_top": 5,
         "link":"https://017.tools/data/User/boyaa/home/desktop/games/basketball/web-mobile/index.html"
@@ -24,7 +28,7 @@ export const PopularGames = [
         "slug": "grand-theft-auto-v",
         "name": "英雄逃亡",
         "released": "2013-09-17",
-        "background_image": "https://api.multiavatar.com/英雄逃亡.png",
+        "background_image": RES.gecao,
         "rating": 4.47,
         "rating_top": 5,
         "link":"https://017.tools/data/User/boyaa/home/desktop/games/roguelike/web-mobile/index.html"
@@ -32,21 +36,12 @@ export const PopularGames = [
     {
         "id": 4,
         "slug": "grand-theft-auto-v",
-        "name": "跳一跳",
+        "name": "奔跑吧企鹅",
         "released": "2013-09-17",
-        "background_image": "https://api.multiavatar.com/跳一跳.png",
+        "background_image": RES.qe,
         "rating": 4.47,
         "rating_top": 5,
-        "link":"https://017.tools/data/User/boyaa/home/desktop/games/web-wangzhetaowang/index.html"
+        "link":"https://017.tools/data/User/boyaa/home/desktop/games/bpxee/web-mobile/index.html"
     },
-    {
-        "id": 5,
-        "slug": "grand-theft-auto-v",
-        "name": "水果消消乐",
-        "released": "2013-09-17",
-        "background_image": "https://api.multiavatar.com/水果消消乐.png",
-        "rating": 4.47,
-        "rating_top": 5,
-        "link":"https://lm.2048.net/fruitgarden/index.html"
-    },
+
 ]
---
+++ src/config/res.js
@@ -0,0 +1,13 @@
+import xjbz from '../assets/xjzb.png'; // 导入图片
+import gecao from '../assets/gecao.png'; // 导入图片
+import qe from '../assets/qe.png'; // 导入图片
+import lq from '../assets/lq.png'; // 导入图片
+
+export const RES = {
+    xjbz:xjbz,
+    qe:qe,
+    lq:lq,
+    gecao:gecao
+}
+
+console.log(RES)
